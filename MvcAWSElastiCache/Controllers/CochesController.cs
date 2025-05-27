49959ffa78a7af91a08a006c3c757d2d4ee43ae0
JorgeRuPa12 <jorgerp12000@gmail.com>
2025-05-27 13:56:58 +0200

    Add project files.

---
+++ MvcAWSElastiCache/Controllers/CochesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSElastiCache.Models;
+using MvcAWSElastiCache.Repositories;
+using MvcAWSElastiCache.Services;
+
+namespace MvcAWSElastiCache.Controllers
+{
+    public class CochesController : Controller
+    {
+        private RepositoryCoches repo;
+        private ServiceCacheRedis service;
+        public CochesController(RepositoryCoches repo, ServiceCacheRedis)
+        {
+            this.repo = repo;
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            List<Coche> coches = this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Coche coche = this.repo.FindCoche(id);
+            return View(coche);
+        }
+
+        public async Task<IActionResult> Favoritos()
+        {
+            List<Coche> coches = await this.service.GetCochesFavoritosAsync();
+            return View(coches);
+        }
+        public async Task<IActionResult> SeleccionarFavoritos(int idcoche)
+        {
+            Coche car = this.repo.FindCoche(idcoche);
+            await this.service.AddFavoritoAsync(car);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteFavorito(int idcoche)
+        {
+            await this.service.DeleteFavoritoAsync(idcoche);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcAWSElastiCache/Services/ServiceCacheRedis.cs
@@ -0,0 +1,63 @@
+﻿using MvcAWSElastiCache.Helpers;
+using MvcAWSElastiCache.Models;
+using Newtonsoft.Json;
+using NuGet.Packaging.Signing;
+using StackExchange.Redis;
+
+namespace MvcAWSElastiCache.Services
+{
+    public class ServiceCacheRedis
+    {
+        private IDatabase cache;
+
+        public ServiceCacheRedis()
+        {
+            this.cache = HelperCacheRedis.Connection.GetDatabase();
+        }
+
+        public async Task<List<Coche>> GetCochesFavoritosAsync()
+        {
+            string jsonCoches = await this.cache.StringGetAsync("favoritos");
+            if(jsonCoches == null)
+            {
+                return null;
+            }
+            else
+            {
+                List<Coche> cars = JsonConvert.DeserializeObject<List<Coche>>(jsonCoches);
+                return cars;
+            }
+        }
+
+        public async Task AddFavoritoAsync(Coche car)
+        {
+            List<Coche> favoritos = await this.GetCochesFavoritosAsync();
+            if(favoritos == null)
+            {
+                favoritos = new List<Coche>();
+            }
+            favoritos.Add(car);
+            string jsonCoches = JsonConvert.SerializeObject(favoritos);
+            await this.cache.StringSetAsync("favoritos", jsonCoches, TimeSpan.FromMinutes(30));
+        }
+
+        public async Task DeleteFavoritoAsync(int idcoche)
+        {
+            List<Coche> favoritos = await this.GetCochesFavoritosAsync();
+            if(favoritos != null)
+            {
+                Coche carDelete = favoritos.FirstOrDefault(x => x.IdCoche == idcoche);
+                favoritos.Remove(carDelete);
+                if(favoritos.Count == 0)
+                {
+                    await this.cache.KeyDeleteAsync("favoritos");
+                }
+                else
+                {
+                    string jsonCoches = JsonConvert.SerializeObject(favoritos);
+                    await this.cache.StringSetAsync("favoritos", jsonCoches, TimeSpan.FromMinutes(30));
+                }
+            }
+        }
+    }
+}
